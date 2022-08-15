7801561205172f28a118a7b969d229cea1557c60
sakanal <sakanal9527@gmail.com>
2022-08-15 17:29:51 +0800

    change!

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -11,9 +11,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(strStr("asdfawqrwe",null));
         System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
     }
 
     public static int strStr(String haystack, String needle) {
