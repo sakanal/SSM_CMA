0e61d5bab9239b6b3febb87aa56ac2f6cddb82d1
sakanal <sakanal9527@gmail.com>
2022-08-15 16:15:40 +0800

    second commit

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -11,6 +11,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(strStr("asdfawqrwe",null));
         System.out.println("test");
+        System.out.println("test");
     }
 
     public static int strStr(String haystack, String needle) {
